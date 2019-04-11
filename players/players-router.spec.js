67228a8f83c92619a6f644eba3ac013b7b5ba074
Max Trestman <mtrestman1@android-4105f9b98c1d7f96.workspace.codeandsupply.co>
2019-04-11 18:50:43 -0400

    Got tests working for POSt, started on Delete, not passing yet

--- players/players-router.spec.js
+++ players/players-router.spec.js
@@ -1,32 +1,47 @@
 const request = require('supertest');
-const server = require('./players-router');
+const server = require('../server');
 
 
 describe('server.js', () => {
     describe('POST', () => {
 
-        it('should return 200 OK', () => {
-            return request(server)
-              .get('/')
-              .then(response => {
+        // it('should return 200 OK', () => {
+        //     return request(server)
+        //       .get('/api/players')
+        //       .then(response => {
+        //         expect(response.status).toBe(200);
+        //       });
+        //   });
+
+        it('should respond with 201 Created', async () => {
+            const body = { name: 'max'}
+            const response = await request(server).post('/api/players').send(body);
+        expect(response.status).toBe(201);
+            
+        })
+
+        it('should respond with 400 If missing req fields', async () => {
+            const body = {}
+            const response = await request(server).post('/api/players').send(body);
+        expect(response.status).toBe(400);
+            
+        })
+
+        it('should return JSON', async () => {
+            const body = { name: 'max'}
+            const response = await request(server).post('/api/players').send(body);
+        expect(response.type).toBe('application/json');
+        })
+
+        })
+
+        describe('Delete', () => {
+
+            it('should return status code 200', async () => {
+                let response = await request(server).delete('/api/players/:id');
                 expect(response.status).toBe(200);
               });
-          });
-
-        it('should respond with 201 Created', () => {
-            return request(server)
-            .post('/')
-            .then(response => {
-                expect(response.status).toBe(201)
-            })
         })
 
         
-
-          
-
-
-
-
-    })
 })
